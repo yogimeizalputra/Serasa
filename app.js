7f2fc9930c718ed1a1a99683d17001aa518b033c
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-12-04 23:16:08 +0700

    Testing Routes & Handling POST Request

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://yogimp:phantom110590@serasa.ln4eb.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
