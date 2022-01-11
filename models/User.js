6110fb4568dc15fbda05b05f211b1e7878ad9b20
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2022-01-11 23:32:29 +0700

    Hashing Passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -23,8 +24,9 @@ UserSchema.post('save', function(doc, next ) {
 })
 
 // fire a function before doc saved to db
-UserSchema.pre('save', function(next) {
-    console.log('user about to be created & saved', this);
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
